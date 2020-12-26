6a7081b9c10930d6e70cfe7396aabb1216dbf589
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2020-12-26 12:08:26 +0200

    add users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+	// TODO
+});
+
+router.post('/', (req, res) => {
+	// TODO
+});
+
+router.put('/:username', (req, res) => {
+	// TODO
+});
+
+router.delete('/:username', (req, res) => {
+	// TODO
+});
+
 module.exports = router;
