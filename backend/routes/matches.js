4653d8c490c0612a2458ffbe8b01acaafccd4fa4
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2020-12-26 12:08:52 +0200

    add matches route

--- backend/routes/matches.js
+++ backend/routes/matches.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const seats = require('./seats');
+
+router.use('/:match_id/seats', seats);
+
+router.get('/', (req, res) => {
+	// TODO
+});
+
+router.get('/:match_id', (req, res) => {
+	// TODO
+});
+
+router.post('/', (req, res) => {
+	// TODO
+});
+
+router.put('/:match_id', (req, res) => {
+	// TODO
+});
+
 module.exports = router;
