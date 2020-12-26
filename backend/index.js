60d86b08ae59968483e2cdbe9c7b4faaa700d9fd
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2020-12-26 11:18:26 +0200

    backend starter code added

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+// dependencies
+const Joi = require('joi');
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const config = require('config'); // e.g: config.get('database.password')
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+const app = express()
+
+// routes modules
+const login = require('./routes/login');
+const users = require('./routes/users');
+const managers = require('./routes/managers');
+const matches = require('./routes/matches');
+const stadiums = require('./routes/stadiums');
+
+// midlewares
+app.use(express.json());
+app.use(helmet());
+
+// routes
+app.use('/api/login', login);
+app.use('/api/users', users);
+app.use('/api/managers/requests', managers);
+app.use('/api/matches', matches);
+app.use('/api/stadiums', stadiums);
+
+// process.env.NODE_ENV
+if (app.get('env') === 'development') {
+	app.use(morgan());
+	startupDebugger('Morgan is enabled...');
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
