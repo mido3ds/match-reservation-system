2e7ac0f115fd3c5f5a11fb8a76dd68318ae06934
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2020-12-26 12:09:07 +0200

    add seats route

---
+++ backend/routes/seats.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	// TODO
+});
+
+router.post('/reserve/:seat_id', (req, res) => {
+	// TODO
+});
+
+router.delete('/:seat_id', (req, res) => {
+	// TODO
+});
+
+module.exports = router;
