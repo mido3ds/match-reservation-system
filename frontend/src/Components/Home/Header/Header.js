637bb8ae3837856e6baf436fff708e7c048b8fc9
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-01-01 23:40:37 +0200

    solve conflicts

--- frontend/src/Components/Home/Header/Header.js
+++ frontend/src/Components/Home/Header/Header.js
@@ -1,10 +1,9 @@
-import '../../Header/Header.css';
+import './Header.css';
 import logo from "../../../images/epl-logo.png";
 
 function Header() {
 
   return (
-    
     <div className="header">
       <img className="header-image" alt="header" src={logo} width="100" height="50"/>
       <div className="text-block">
