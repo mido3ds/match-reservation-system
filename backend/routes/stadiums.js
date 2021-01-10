b42c336c48e2c92cdec2a13d30191dd20faa2353
Mahmoud Adas <mido3ds@gmail.com>
2021-01-10 21:32:13 +0200

    backend: added stadiums route

--- backend/routes/stadiums.js
+++ backend/routes/stadiums.js
@@ -1,13 +1,41 @@
 const express = require('express');
+const { Stadium, validate } = require('../models/stadium');
+const auth = require('../middleware/auth');
+const manager = require('../middleware/manager');
+const _ = require('lodash');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // TODO
+router.get('/', async (req, res) => {
+  const pageSize = 10;
+  if (req.params.page < 1) return res.status(406).send('not acceptable page < 1');
+
+  const stadiums = await Stadium
+    .aggregate([{
+      $project: {
+        _id: 0, uuid: "$_id",
+        name: 1, city: 1,
+      }
+    }])
+    .skip((req.query.page - 1) * pageSize)
+    .limit(pageSize)
+
+  res.send(stadiums);
 });
 
-router.post('/', (req, res) => {
-  // TODO
+router.post('/', [auth, manager], async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(403).send(error.details[0].message);
+
+  let stadium = await Stadium.findOne({ name: req.body.name });
+  if (stadium) return res.status(403).send('This stadium already exists.');
+
+  const to_pick = ['name', 'city'];
+  stadium = new Stadium(_.pick(req.body, to_pick));
+  console.log(stadium);
+
+  await stadium.save();
+  res.send({})
 });
 
 module.exports = router;
