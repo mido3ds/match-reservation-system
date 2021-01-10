545710e0ab7f351bc92f81fe5f143480188f291b
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-01-10 22:20:15 +0200

    home header added

---
+++ frontend/src/Components/Home/Card/HomeCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomeCard extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+            </div>
+         );
+    }
+}
+ 
+export default HomeCard;
--- frontend/src/Components/Home/HomeHeader/HomeHeader.jsx
+++ frontend/src/Components/Home/HomeHeader/HomeHeader.jsx
@@ -1,11 +1,12 @@
 import logo from "../../../images/epl-logo.png";
+import Ball from "../../../images/ball.jpg";
 import './HomeHeader.css';
 
 function HomeHeader() {
 
   return (
     <div className="header">
-      <img className="header-image" alt="header" src={logo} width="100" height="50" />
+      <img className="header-image" alt="header" src={Ball} />
       <div className="text-block">
         <h1> Welcome To EPL Website </h1>
         <p> Here you can reserve your tickets and follow your favorite matches </p>
