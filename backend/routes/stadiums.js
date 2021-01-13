78a6d482b86d603c40c52d4362a0ce412d667509
Mahmoud Adas <mido3ds@gmail.com>
2021-01-13 13:50:08 +0200

    stadiums return err

--- backend/routes/stadiums.js
+++ backend/routes/stadiums.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   const pageSize = 10;
-  if (req.params.page < 1) return res.status(406).send('not acceptable page < 1');
+  if (req.params.page < 1) return res.status(406).send({ err: 'not acceptable page < 1' });
 
   const stadiums = await Stadium
     .aggregate([{
@@ -25,10 +25,10 @@ router.get('/', async (req, res) => {
 
 router.post('/', [auth, manager], async (req, res) => {
   const { error } = validate(req.body);
-  if (error) return res.status(403).send(error.details[0].message);
+  if (error) return res.status(403).send({ err: error.details[0].message });
 
   let stadium = await Stadium.findOne({ name: req.body.name });
-  if (stadium) return res.status(403).send('This stadium already exists.');
+  if (stadium) return res.status(403).send({ err: 'This stadium already exists.' });
 
   const to_pick = ['name', 'city'];
   stadium = new Stadium(_.pick(req.body, to_pick));
