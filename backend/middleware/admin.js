e3b88b7dc9e60d9c0609b17c53328aa6a655ac6a
Yosry Ahmed <yosrym93@gmail.com>
2021-01-13 19:34:49 +0200

    Fixed a bug in admin authentication

--- backend/middleware/admin.js
+++ backend/middleware/admin.js
@@ -1,7 +1,7 @@
 function admin(req, res, next) {
   // 401 unauthorized
   // 403 forbidden
-  if (!req.user.role == "admin") return res.status(403).send('Access denied');
+  if (req.user.role != "admin") return res.status(403).send('Access denied');
   next();
 }
 
