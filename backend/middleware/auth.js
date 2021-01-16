dd93785666a131da2770b7ec0ba7588db7dfcc27
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-01-16 22:58:42 +0200

    add async to middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-function auth(req, res, next) {
+async function auth(req, res, next) {
   const token = req.header('x-auth-token');
   if (!token) return res.status(401).send({ err: 'No token' });
 
