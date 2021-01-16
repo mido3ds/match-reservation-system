6f9027d188908b679450917d83cc7d9906e8b36f
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-01-16 22:47:19 +0200

    unauthorized when the user is deleted

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,8 +8,15 @@ function auth(req, res, next) {
   try {
     const decoded = jwt.verify(token, config.get('jwt_private_key'));
     req.user = decoded;
-    // TODO: make sure the user still exists in the datatbase (could be deleted by the admin)
-    next();
+    // make sure the user still exists in the datatbase (could be deleted by the admin)
+    let user;
+    try {
+      user = await User.findById(req.user._id);
+    } catch (err) {
+      return res.status(500).send({ err: err.message });
+    }
+    if (user) next();
+    res.status(400).send({ err: 'Invalid token!' });
   } catch (ex) {
     return res.status(400).send({ err: 'Invalid token!' });
   }
