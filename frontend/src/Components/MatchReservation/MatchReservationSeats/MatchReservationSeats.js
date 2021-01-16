763411dca52e7841a403c02d86cd40a78c86475d
AhmedMahmoud98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2021-01-16 17:46:36 +0200

    Replace ticket card hardcoded values with values returned from the backend

--- frontend/src/Components/MatchReservation/MatchReservationSeats/MatchReservationSeats.js
+++ frontend/src/Components/MatchReservation/MatchReservationSeats/MatchReservationSeats.js
@@ -67,7 +67,7 @@ export default class MatchReservationSeats extends Component {
     }
 
     const rows = [
-      [{id: 1, number:1}, {id: 2, number:2}, {id: 3, number:3, isReserved: true}, {id: 4, number:4}, {id: 5, number:5}, {id: 6, number:6}, {id: 7, number:7}, {id: 8, number:8}, {id: 9, number:9}, {id: 10, number:10}],
+      [{id: "A1"}, {id: "A2", number:2}, {id: "A3", number:3, isReserved: true}, {id: 4, number:4}, {id: 5, number:5}, {id: 6, number:6}, {id: 7, number:7}, {id: 8, number:8}, {id: 9, number:9}, {id: 10, number:10}],
       [{id: 11, number:1, isReserved: true}, {id: 12, number:2, isReserved: true}, {id: 13, number:3, isReserved: true}, {id: 14, number:4}, {id: 15, number:5}, {id: 16, number:6}, {id: 17, number:7}, {id: 18, number:8}, {id: 19, number:9}, {id: 20, number:10}],
       [{id: 21, number:1}, {id: 22, number:2}, {id: 23, number:3, isReserved: true}, {id: 24, number:4}, {id: 25, number:5}, {id: 26, number:6}, {id: 27, number:7}, {id: 28, number:8}, {id: 29, number:9}, {id: 30, number:10}]
     ]
@@ -82,7 +82,7 @@ export default class MatchReservationSeats extends Component {
             ))}
           </div> 
         <div className = "seats-area">
-            <SeatPicker
+            <SeatPicker 
               addSeatCallback={this.addSeatCallback}
               removeSeatCallback={this.removeSeatCallback}
               rows={rows}
@@ -100,7 +100,7 @@ export default class MatchReservationSeats extends Component {
           <h2 className="tickets-title"> Tickets: </h2>
           <div className="flex-container-row-vcenter">
             {tickets.map(ticket => (
-              <div className="ticket-area flex-container-column-hcenter">
+              <div key={ticket.id.toString()} className="ticket-area flex-container-column-hcenter">
                 <img alt="reject-icon" className="cancel-ticket" src="https://www.flaticon.com/svg/static/icons/svg/58/58253.svg" />
                 <img className="ticket-image" alt="pitch" src={Ticket}/>
                 <div className="flex-container-row-hcenter">
